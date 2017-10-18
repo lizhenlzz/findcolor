2ce5d40a34e5d8bb045b2cc898bb49f8735c959f
lizhenlzz <you@example.com>
2017-10-18 12:00:01 +0800

    updata

--- ColorView.js
+++ ColorView.js
@@ -1,13 +1,5 @@
 window.findColor=window.findColor||{};
 (function(){
-	/*
-	 * info   字面量形式     background. opration. row. cotainerWidth
-	 */
-//	    try{控制形参的方法   在可能出现错误的地方添加try catch
-//			console.log(info.background)
-//		}catch(error){
-//			console.log(error)
-//		}//保证不会报错崩溃
 	function ColorView(info){
 		var _info=info||{};//确保参数存在
 		var row=_info.row//行数
@@ -17,11 +9,5 @@ window.findColor=window.findColor||{};
 		this.view=document.createElement("div")
 	    this.view.style.cssText="background-color:"+_info.background+";opacity:"+_info.opacity+";float:left;width:"+width+"px;height:"+width+"px;margin:"+space+"px 0 0 "+space+"px;"
 	}
-	
-	
-	
-	
-	
-	
 	findColor.ColorView=ColorView;
 })()
